387d4c501a829f968fe043cbf6d1b969b2db2614
your name <your@email.here>
2024-03-14 18:06:17 -0400

    create plant  unit test

--- api/tests/test_plants.py
+++ api/tests/test_plants.py
@@ -2,10 +2,12 @@ from fastapi.testclient import TestClient
 from main import app
 from queries.plants import PlantQueries
 from authenticator import authenticator
+
 client = TestClient(app=app)
 
+
 class MockPlantQueries:
-    def get_all_plants(self,account_id):
+    def get_all_plants(self, account_id):
         return [
             {
                 "id": "##",
@@ -17,15 +19,27 @@ class MockPlantQueries:
             }
         ]
 
+    def create(self, plant_in, account_id):
+        return {
+            "id": "##",
+            "name": "bar",
+            "species": "foo",
+            "picture_url": "earl",
+            "detail": "hello",
+            "account_id": "##",
+        }
+
+
 def mock_get_current_account_data():
-    return {
-        "id": "FAKE_ACCOUNT_ID"
-    }
+    return {"id": "FAKE_ACCOUNT_ID"}
+
 
 def test_get_all_plants():
     # Arrange
     app.dependency_overrides[PlantQueries] = MockPlantQueries
-    app.dependency_overrides[authenticator.get_current_account_data]=  (mock_get_current_account_data)
+    app.dependency_overrides[authenticator.get_current_account_data] = (
+        mock_get_current_account_data
+    )
     # Act
     res = client.get("/api/mine/plants")
     # Assert
@@ -34,11 +48,37 @@ def test_get_all_plants():
         "plants": [
             {
                 "id": "##",
-                "name":"bar",
+                "name": "bar",
                 "species": "foo",
                 "picture_url": "earl",
                 "detail": "hello",
                 "account_id": "##",
             }
         ]
+    }
+
+
+def test_create_plant():
+    # Arrange
+    app.dependency_overrides[PlantQueries] = MockPlantQueries
+    app.dependency_overrides[authenticator.get_current_account_data] = (
+        mock_get_current_account_data
+    )
+    # Act
+    plant_in = {
+        "name": "bar",
+        "species": "foo",
+        "picture_url": "earl",
+        "detail": "hello",
+    }
+    res = client.post("/api/plants", json=plant_in)
+    # Assert
+    assert res.status_code == 200
+    assert res.json() == {
+        "id": "##",
+        "name": "bar",
+        "species": "foo",
+        "picture_url": "earl",
+        "detail": "hello",
+        "account_id": "##",
     }
