4dd7eb1703accfef67569b8e673a435f610c36dd
Anthony Nguyen <anguye18@fau.edu>
2024-03-20 10:19:47 -0400

    onboard design

--- ghi/src/pages/Onboard.jsx
+++ ghi/src/pages/Onboard.jsx
@@ -12,47 +12,57 @@ const Onboard = () => {
 
     return (
         <div
-            className=""
+            className="position-relative"
             style={{
-                backgroundColor: '#30694C',
                 backgroundSize: 'cover',
+                backgroundImage: "url('https://i.imgur.com/JmTCvtI.jpeg')",
                 width: '100vw',
                 height: '100vh',
-                paddingRight: '20px',
+                paddingRight: '25px',
             }}
         >
-            <div
-                className="container"
-                style={{
-                    backgroundColor: '#f5faf6',
-                    backgroundSize: 'cover',
-                    height: '100vh',
-                }}
-            >
-                <br></br>
-                <br></br>
-                <div className="d-flex justify-content-center">
-                    <img
-                        className="img-fluid"
-                        width="45%"
-                        src="https://i.imgur.com/Ri3xfZG.png"
-                    />
+            <div className="position-absolute top-50 start-50 translate-middle">
+                <div
+                    className="rounded"
+                    style={{
+                        backgroundColor: 'black',
+                        background: 'rgba(51, 79, 60, 0.7)',
+                        boxShadow:
+                            'inset 0 0 20px rgba(0, 0, 0, 0.4), inset 0 0 40px rgba(255, 255, 255, 0.1)',
+                    }}
+                >
+                    <div className="d-flex justify-content-center p-4">
+                        <h5
+                            className="display-1 "
+                            style={{
+                                fontFamily: "'Abril Fatface', serif",
+                                fontWeight: '400',
+                                fontStyle: 'normal',
+                                color: '#F7F7F2',
+                            }}
+                        >
+                            Plant memories
+                        </h5>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <h6
+                            className="display-2 text-white"
+                            style={{
+                                fontFamily: "'Cormorant Garamond', serif",
+                                fontWeight: '600',
+                                fontStyle: 'italic',
+                            }}
+                        >
+                            one entry at a time
+                        </h6>
+                    </div>
                 </div>
-                <div className="d-flex justify-content-center">
-                    <h5 className="display-6">Plant memories,</h5>
-                </div>
-                <div className="d-flex justify-content-center">
-                    <h6 className="display-6 cormorant-garamond-light">
-                        one entry at a time
-                    </h6>
-                </div>
                 <br></br>
-
                 <div
                     className="d-flex justify-content-center"
                     style={{ gap: '5px' }}
                 >
-                    <button onClick={signupClick} className="btn btn-success">
+                    <button onClick={signupClick} className="btn btn-light">
                         Signup
                     </button>
                     <button onClick={loginClick} className="btn btn-success">
